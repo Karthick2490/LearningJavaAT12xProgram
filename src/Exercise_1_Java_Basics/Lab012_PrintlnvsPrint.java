1ab8b3fae6ae46a34692d91116bb1d0e83336f0f
karthickudayakumar <karthick2490@gmail.com>
2025-05-15 23:34:07 +0530

    Lab014_Commit and Push

---
+++ src/Exercise_1_Java_Basics/Lab012_PrintlnvsPrint.java
@@ -0,0 +1,10 @@
+package Exercise_1_Java_Basics;
+
+public class Lab012_PrintlnvsPrint {
+    public static void main(String[] args) {
+        System.out.println("Karthick");
+        System.out.println("Udayakumar");
+        System.out.print("Karthick");
+        System.out.print("Udayakumar");
+    }
+}
