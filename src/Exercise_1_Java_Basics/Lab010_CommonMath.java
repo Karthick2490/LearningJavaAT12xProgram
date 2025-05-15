622a288fe38e32cfaaa74e17435355ab8229f30e
karthickudayakumar <karthick2490@gmail.com>
2025-05-15 23:27:36 +0530

    Lab010_Commit and Push

---
+++ src/Exercise_1_Java_Basics/Lab010_CommonMath.java
@@ -0,0 +1,15 @@
+package Exercise_1_Java_Basics;
+/* Math Operations
+**Author: Karthick
+*/
+
+public class Lab010_CommonMath {
+    public static void main(String[] args) {
+        System.out.println(2*2);
+        System.out.println(2+2);
+        System.out.println(5-2);
+        System.out.println(4/2);
+        System.out.println(3/2); //Prints only Integer not decimal
+        System.out.println("2*2"); //Printing the string
+    }
+}
