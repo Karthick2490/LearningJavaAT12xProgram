27a9ff426e98d5dc9fff7316195b885faa21adcf
karthickudayakumar <karthick2490@gmail.com>
2025-05-15 23:31:56 +0530

    Lab013_Commit and Push

---
+++ src/Exercise_1_Java_Basics/Lab011_MathExcpetions.java
@@ -0,0 +1,7 @@
+package Exercise_1_Java_Basics;
+
+public class Lab011_MathExcpetions {
+    public static void main(String[] args) {
+        System.out.println(10/0); //Exception in thread "main" java.lang.ArithmeticException: / by zero
+    }
+}
