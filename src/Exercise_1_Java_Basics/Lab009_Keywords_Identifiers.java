9dbfd8aafded2a6aa2018dd474ee166110ee03a8
karthickudayakumar <karthick2490@gmail.com>
2025-05-15 23:23:13 +0530

    Lab009_Commit and Push

---
+++ src/Exercise_1_Java_Basics/Lab009_Keywords_Identifiers.java
@@ -0,0 +1,10 @@
+package Exercise_1_Java_Basics;
+
+public class Lab009_Keywords_Identifiers {
+    public static void main(String[] args) {
+        System.out.println("Hi Keyword");
+        int Sum = 0; //Invalid
+        int sum = 10;
+        System.out.println(sum);
+    }
+}
