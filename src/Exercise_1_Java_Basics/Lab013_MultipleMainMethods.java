ccc98b38348da2f830360a46381d47b5627c6f45
karthickudayakumar <karthick2490@gmail.com>
2025-05-15 23:36:15 +0530

    Lab015_Commit and Push

---
+++ src/Exercise_1_Java_Basics/Lab013_MultipleMainMethods.java
@@ -0,0 +1,19 @@
+package Exercise_1_Java_Basics;
+
+public class Lab013_MultipleMainMethods {
+    public static void main(String[] args) {
+        System.out.println("Main Method - Method Overloading Concepts");
+    }
+
+    public static void main(String args) {
+
+    }
+
+    public static void main(int[] args) {
+
+    }
+
+    public static void main(int args) {
+
+    }
+}
