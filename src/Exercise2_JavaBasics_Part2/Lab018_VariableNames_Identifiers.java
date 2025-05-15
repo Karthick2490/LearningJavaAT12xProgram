e9193011be714018077398415a743159538990ec
karthickudayakumar <karthick2490@gmail.com>
2025-05-16 00:04:56 +0530

    Lab017_Commit and Push

---
+++ src/Exercise2_JavaBasics_Part2/Lab018_VariableNames_Identifiers.java
@@ -0,0 +1,13 @@
+package Exercise2_JavaBasics_Part2;
+
+public class Lab018_VariableNames_Identifiers {
+    public static void main(String[] args) {
+        int age = 65;
+        int _ = 10;
+        int $ = 2;
+        int Year = 2024;
+        int year = 2022;
+        int _2 = 80;
+        int longvariablenamecalledasidentifier_$ = 90;
+    }
+}
