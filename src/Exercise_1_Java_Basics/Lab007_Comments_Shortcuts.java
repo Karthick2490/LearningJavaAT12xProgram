a45bdecf3f6bc6bdcd873fd1bee493d5dfcae96d
karthickudayakumar <karthick2490@gmail.com>
2025-05-15 23:06:26 +0530

    Lab007_Commit and Push

---
+++ src/Exercise_1_Java_Basics/Lab007_Comments_Shortcuts.java
@@ -0,0 +1,17 @@
+package Exercise_1_Java_Basics;
+
+public class Lab007_Comments_Shortcuts {
+    public static void main(String[] args) {
+        System.out.println("Hello Karthick");
+        System.out.println("Hello Karthick");
+        System.out.println("Hello Karthick");
+        System.out.println("Hello Karthick");
+        System.out.println("Hello Karthick");
+        System.out.println("Hello Karthick");
+        System.out.println("Hello Karthick");
+        System.out.println("Hello Karthick");
+        System.out.println("Hello Karthick");
+        System.out.println("Hello Karthick");
+        System.out.println("Hello Karthick");
+    }
+}
