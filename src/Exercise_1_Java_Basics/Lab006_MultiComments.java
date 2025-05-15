7ceb15301925138d562f3ccb769e6d6081de2625
karthickudayakumar <karthick2490@gmail.com>
2025-05-15 23:04:56 +0530

    Lab006_Commit and Push

---
+++ src/Exercise_1_Java_Basics/Lab006_MultiComments.java
@@ -0,0 +1,29 @@
+package Exercise_1_Java_Basics;
+/*
+MultiComment Learning
+Author: Karthick Udayakumar
+Date: 15 May 2025
+Time: 11:01PM
+ */
+
+public class Lab006_MultiComments {
+    /*
+**MultiComment Learning
+**Author: Karthick Udayakumar
+**Date: 15 May 2025
+**Time: 11:01PM
+ */
+    public static void main(String[] args) {
+        System.out.println("Multi Comments");
+    }
+}
+/*
+**MultiComment Learning
+**Author: Karthick Udayakumar
+**Date: 15 May 2025
+        **Time: 11:01PM
+ */
+
+/*
+*** Multiline Comment ****
+*/
