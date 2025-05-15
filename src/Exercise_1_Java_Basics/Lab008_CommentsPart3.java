bead1c5c33d18cc5a8f2040599da18cbc5b6e17e
karthickudayakumar <karthick2490@gmail.com>
2025-05-15 23:09:17 +0530

    Lab008_Commit and Push

---
+++ src/Exercise_1_Java_Basics/Lab008_CommentsPart3.java
@@ -0,0 +1,14 @@
+package Exercise_1_Java_Basics;
+/*
+Author: Karthick Udayakumar
+Date: 15 May 11:08PM IST
+ */
+public class Lab008_CommentsPart3 {
+    public static void main(String[] args) {
+
+        //Commenting the code
+        //System.out.println("This is a comments");
+        //System.out.println("This is a comments");
+        System.out.println("This is a comment");
+    }
+}
