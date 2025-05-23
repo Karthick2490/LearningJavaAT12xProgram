24b221bf58afc6ef7a423a54aef632f17ac83231
karthickudayakumar <karthick2490@gmail.com>
2025-05-23 23:37:04 +0530

    May 23 Changes - Tasks and Exercises

---
+++ src/Tasks/Task1_Printf.java
@@ -0,0 +1,28 @@
+package Tasks;
+
+public class Task1_Printf {
+    public static void main(String[] args) {
+        //Print 5 table like this 5 X 1 = 5 till 5X10=50 vertically
+        int number = 5;
+        System.out.printf("%d x %d = %d", number, 1, number*1);
+        System.out.println();
+        System.out.printf("%d x %d = %d", number, 2, number*2);
+        System.out.println();
+        System.out.printf("%d x %d = %d", number, 3, number*3);
+        System.out.println();
+        System.out.printf("%d x %d = %d", number, 4, number*4);
+        System.out.println();
+        System.out.printf("%d x %d = %d", number, 5, number*5);
+        System.out.println();
+        System.out.printf("%d x %d = %d", number, 6, number*6);
+        System.out.println();
+        System.out.printf("%d x %d = %d", number, 7, number*7);
+        System.out.println();
+        System.out.printf("%d x %d = %d", number, 8, number*8);
+        System.out.println();
+        System.out.printf("%d x %d = %d", number, 9, number*9);
+        System.out.println();
+        System.out.printf("%d x %d = %d", number, 10, number*10);
+        System.out.println();
+    }
+}
