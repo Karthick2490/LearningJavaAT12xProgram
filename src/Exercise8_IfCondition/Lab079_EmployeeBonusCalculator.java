beb59e68aad33155755832440fa28748f4b8874a
karthickudayakumar <karthick2490@gmail.com>
2025-05-27 23:00:43 +0530

    May 27 Changes - Tasks - 27 May

--- src/Exercise8_IfCondition/Lab079_EmployeeBonusCalculator.java
+++ src/Exercise8_IfCondition/Lab079_EmployeeBonusCalculator.java
@@ -5,52 +5,70 @@ import java.util.Scanner;
 public class Lab079_EmployeeBonusCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the Performance Rating");
-        int performanceRating = 0;
-        int year = 0;
+        int year;
+        int rating;
         float bonus = 0;
-        while(performanceRating > 3)
+        System.out.println("Enter the Performance Rating");
+        while(true)
         {
-            System.out.println("Enter a valid Performance Rating");
-            scanner.next();
+            if(scanner.hasNextInt())
+            {
+                rating = scanner.nextInt();
+                if(rating >= 1 && rating <= 3)
+                {
+                    break;
+                }
+                else {
+                    System.out.println("Enter a Valid Performance Rating between 1 to 3");
+                }
+            }
+            else {
+                System.out.println("Please check your input and enter a valid performance Rating");
+            }
         }
-        performanceRating = scanner.nextInt();
-        while(!scanner.hasNextInt())
+        System.out.println("Enter the Year of Service");
+        while(true)
         {
-            System.out.println("Enter a Valid Performance Rating [1 or 2 or 3");
-            scanner.next();
+            if(scanner.hasNextInt())
+            {
+                year = scanner.nextInt();
+                if(year >=0)
+                {
+                    break;
+                }
+                else {
+                    System.out.println("Enter a Valid Year of service");
+                }
+            }
+            else {
+                System.out.println("Please check your input and enter a valid year of Service");
+            }
         }
-        performanceRating = scanner.nextInt();
-        System.out.println("Enter the Year of Experience");
-        while(!scanner.hasNextInt())
-        {
-            System.out.println("Enter a Valid Year of Experience");
-            scanner.next();
-            year = scanner.nextInt();
-        }
-        if(performanceRating == 1)
+
+        if(rating == 1)
         {
             bonus = 5000;
         }
-        else if(performanceRating == 2)
+        else if(rating == 2)
         {
             bonus = 3000;
         }
-        else if(performanceRating == 3)
+        else
         {
             bonus = 1000;
         }
+
         if(year >=5)
         {
             bonus *= 1.5F;
         }
-        else if(year>=3 && year < 5)
-        {
+        else if(year >=3){
             bonus *= 1.2F;
         }
-        else {
+        else
+        {
             bonus *= 1F;
         }
-        System.out.println("Bonus: $" + bonus);
+        System.out.println("Bonus: $" +bonus);
     }
 }
