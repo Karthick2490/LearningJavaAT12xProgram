94fda3f19b557aaee1a78cf2ad54b54fb5f94390
karthickudayakumar <karthick2490@gmail.com>
2025-06-21 11:34:42 +0530

    June 21 Changes - Assessments2

---
+++ src/Assesments/Challenge2.java
@@ -0,0 +1,20 @@
+package Assesments;
+
+public class Challenge2 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        int addition = a + b;
+        int substraction = a - b;
+        int multiplication = a * b;
+        int division = a / b;
+        int modulus = a % b;
+
+        System.out.println("Addition: " +addition);
+        System.out.println("Subtraction: " +substraction);
+        System.out.println("Multiplication: " +multiplication);
+        System.out.println("Division: " +division);
+        System.out.println("Modulus: " +modulus);
+    }
+}
