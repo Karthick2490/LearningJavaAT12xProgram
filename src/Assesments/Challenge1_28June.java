43b6353bea222805eda45b9b94101f1b3f4f2a6b
karthickudayakumar <karthick2490@gmail.com>
2025-06-28 11:06:46 +0530

    June 28 Changes - Assessment

---
+++ src/Assesments/Challenge1_28June.java
@@ -0,0 +1,10 @@
+package Assesments;
+
+public class Challenge1_28June {
+    public static void main(String[] args) {
+        String input = "Hello World";
+        System.out.println("Length: " + input.length());
+        System.out.println("First char: " + input.substring(0,1));
+        System.out.println("Substring: " + input.substring(6));
+    }
+}
