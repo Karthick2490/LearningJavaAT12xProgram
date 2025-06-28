91346ee32a1a11977f3a58ef68be0aa7ff28d3a6
karthickudayakumar <karthick2490@gmail.com>
2025-06-28 11:17:07 +0530

    June 28 Changes - Assessment

---
+++ src/Assesments/Challenge2_28June.java
@@ -0,0 +1,14 @@
+package Assesments;
+
+public class Challenge2_28June {
+    public static void main(String[] args) {
+        String String1 = "Hello";
+        String String2 =  "hello";
+        String String3 = "Hello";
+
+        System.out.println("equals(): " + String1.equals(String2));
+        System.out.println("equalsIgnoreCase(): " + String1.equalsIgnoreCase(String2));
+        System.out.println("CompareTo(): " + String3.compareTo(String2));
+
+    }
+}
